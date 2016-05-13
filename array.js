e38ea29b5808e0c26a122d43e40e559b8ee38d1a
rclauson <rclauson@google.com>
2016-05-13 12:38:01 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,13 @@
 'use strict';
+function createArrays(){
+    var snickers=0;
+    var hundred =1;
+    var grand =2;
+    var kitkat=3;
+    var skittles=4;
+    return [snickers, hundred, grand, kitkat, skittles];
+};
+
+function createArrays (){
+  [snickers, hundred, grand, kitkat, skittles] = createArrays();
+}
